6c14f5573bd216d3bf22cf05929585ed4c4e4916
Mahmoud <m4m205@users.noreply.github.com>
2017-10-16 17:24:47 +0200

    with ted

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ to_do_controller(app);
 
 
 
-app.listen(process.env.port || 3000 );
-
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 
